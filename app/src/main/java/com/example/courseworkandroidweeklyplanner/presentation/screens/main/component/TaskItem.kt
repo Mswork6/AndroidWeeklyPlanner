a16d70c54f215a5582f89d03ed8471a2e740a8f5
Mswork6 <denis.ch74@mail.ru>
2025-05-11 20:57:57 +0500

    feat: made new taskItems and improved main TaskItem

--- app/src/main/java/com/example/courseworkandroidweeklyplanner/presentation/screens/main/component/TaskItem.kt
+++ app/src/main/java/com/example/courseworkandroidweeklyplanner/presentation/screens/main/component/TaskItem.kt
@@ -1,9 +1,15 @@
 package com.example.courseworkandroidweeklyplanner.presentation.screens.main.component
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CardDefaults
@@ -11,12 +17,14 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.courseworkandroidweeklyplanner.R
@@ -42,69 +50,79 @@ fun TaskItem(
     ),
     onClick = onClick,
     modifier = modifier,
-    contentModifier = Modifier
-        .padding(
-            top = 16.dp,
-            bottom = 16.dp,
-            start = 8.dp,
-            end = 8.dp
-        )
 ) {
-    Icon(
-        imageVector = when (task.isDone) {
-            true -> ImageVector.vectorResource(R.drawable.icon_checkbox_done)
-            false -> ImageVector.vectorResource(R.drawable.baseline_check_box_outline_blank_24)
-        },
-        contentDescription = stringResource(R.string.description_task_check_box),
-        tint = if (task.isDone) {
-            Color.Unspecified
-        } else {
-            when (task.priority) {
-                Priority.LOW -> colorResource(R.color.gray)
-                Priority.BASIC -> colorResource(R.color.black)
-                Priority.HIGH -> colorResource(R.color.red)
-            }
-        },
+    val stripeColor = when (task.difficulty) {
+        Difficulty.HARD   -> colorResource(R.color.red)
+        Difficulty.MEDIUM -> colorResource(R.color.orange)
+        Difficulty.EASY   -> colorResource(R.color.green)
+    }
+    val stripeWidth = 12.dp
+
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(IntrinsicSize.Min))
+    {
+        // Used to write category color stripe
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(stripeWidth)
+                .align(Alignment.CenterEnd)
+                .background(stripeColor)
         )
-    Spacer(modifier = Modifier.width(4.dp))
-    Text(text = task.category.emoji)
-    Spacer(modifier = Modifier.width(4.dp))
-    Text(
-        text = task.name,
-        modifier = Modifier
-            .weight(1f),
-        style = MaterialTheme.typography.bodyMedium
-    )
-    Spacer(modifier = Modifier.width(8.dp))
-    Text(
-        text = timeToString(task.time),
-        style = MaterialTheme.typography.bodyMedium
-    )
-
-//    if (task.time != null) {
-//        Icon(
-//            imageVector = Icons.Default.Notifications,
-//            contentDescription = stringResource(R.string.description_time),
-//            modifier = Modifier.weight(0.5f)
-//        )
-//    } OUTDATED (May use later)
-
-    Spacer(modifier = Modifier.width(8.dp))
-    Row {
-        when (task.difficulty) {
-            Difficulty.HARD -> BatteryIcon(color = colorResource(R.color.red))
-            Difficulty.MEDIUM -> BatteryIcon(
-                bodyCoefficient = 0.5f,
-                color = colorResource(R.color.orange)
-            )
-
-            Difficulty.EASY -> BatteryIcon(
-                bodyCoefficient = 0.25f,
-                color = colorResource(R.color.green)
-            )
+        // Main content
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = 16.dp,
+                    bottom = 16.dp,
+                    start = 8.dp,
+                    end = stripeWidth + 4.dp)
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    imageVector = if (task.isDone)
+                        ImageVector.vectorResource(R.drawable.icon_checkbox_done)
+                    else
+                        ImageVector.vectorResource(R.drawable.baseline_check_box_outline_blank_24),
+                    contentDescription = null,
+                    tint = if (task.isDone) Color.Unspecified else when (task.priority) {
+                        Priority.LOW   -> colorResource(R.color.gray)
+                        Priority.BASIC -> colorResource(R.color.black)
+                        Priority.HIGH  -> colorResource(R.color.red)
+                    }
+                )
+                Spacer(Modifier.width(4.dp))
+                Text(
+                    text = task.name,
+                    modifier = Modifier.weight(1f),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = task.category.emoji,
+                    modifier = Modifier
+                        .width(24.dp),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Spacer(Modifier.width(8.dp))
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.baseline_access_time_16),
+                    contentDescription = null
+                )
+                Spacer(Modifier.width(4.dp))
+                Text(
+                    text = timeToString(task.time),
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
         }
     }
-
 }
 
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
@@ -119,7 +137,7 @@ private fun TaskCardWithIconPreview1() {
                 name = "Отвезти бананы в грузию",
                 description = "Нужно сесть в грузовик и привезти бананы",
                 priority = Priority.HIGH,
-                difficulty = Difficulty.MEDIUM,
+                difficulty = Difficulty.EASY,
                 category = Category.WORK,
                 date = LocalDate.of(2024, 10, 13),
                 time = LocalTime.of(17, 33),
@@ -129,27 +147,3 @@ private fun TaskCardWithIconPreview1() {
         )
     }
 }
-
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-private fun TaskCardWithIconPreview2() {
-    CourseWorkAndroidWeeklyPlannerTheme {
-        TaskItem(
-            onClick = { },
-            task = Task(
-                id = UUID.randomUUID(),
-                name = "Отвезти бананы в грузию",
-                description = "Нужно сесть в грузовик и привезти бананы",
-                priority = Priority.HIGH,
-                difficulty = Difficulty.MEDIUM,
-                category = Category.WORK,
-                date = LocalDate.of(2024, 10, 13),
-                time = LocalTime.of(23, 30),
-                isDone = true
-            ),
-            modifier = Modifier.fillMaxWidth()
-        )
-    }
-}
-
--- app/src/main/java/com/example/courseworkandroidweeklyplanner/presentation/screens/main/component/TaskItemTest1.kt
+++ app/src/main/java/com/example/courseworkandroidweeklyplanner/presentation/screens/main/component/TaskItemTest1.kt
@@ -1,8 +1,6 @@
 package com.example.courseworkandroidweeklyplanner.presentation.screens.main.component
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,16 +11,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.courseworkandroidweeklyplanner.R
@@ -37,7 +31,7 @@ import java.time.LocalTime
 import java.util.UUID
 
 @Composable
-fun TaskItemTest(
+fun TaskItemTest1(
     task: Task,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
@@ -56,34 +50,37 @@ fun TaskItemTest(
             end = 8.dp
         )
 ) {
-    Column {
-        Row(
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                imageVector = when (task.isDone) {
-                    true -> ImageVector.vectorResource(R.drawable.icon_checkbox_done)
-                    false -> ImageVector.vectorResource(R.drawable.baseline_check_box_outline_blank_24)
-                },
-                contentDescription = stringResource(R.string.description_task_check_box),
-                tint = if (task.isDone) {
-                    Color.Unspecified
-                } else {
-                    when (task.priority) {
-                        Priority.LOW -> colorResource(R.color.gray)
-                        Priority.BASIC -> colorResource(R.color.black)
-                        Priority.HIGH -> colorResource(R.color.red)
-                    }
-                },
-            )
-            Spacer(modifier = Modifier.width(4.dp))
+    Icon(
+        imageVector = when (task.isDone) {
+            true -> ImageVector.vectorResource(R.drawable.icon_checkbox_done)
+            false -> ImageVector.vectorResource(R.drawable.baseline_check_box_outline_blank_24)
+        },
+        contentDescription = stringResource(R.string.description_task_check_box),
+        tint = if (task.isDone) {
+            Color.Unspecified
+        } else {
+            when (task.priority) {
+                Priority.LOW -> colorResource(R.color.gray)
+                Priority.BASIC -> colorResource(R.color.black)
+                Priority.HIGH -> colorResource(R.color.red)
+            }
+        },
+        )
+    Spacer(modifier = Modifier.width(4.dp))
+    Text(text = task.category.emoji)
+    Spacer(modifier = Modifier.width(4.dp))
+    Text(
+        text = task.name,
+        modifier = Modifier
+            .weight(1f),
+        style = MaterialTheme.typography.bodyMedium
+    )
+    Spacer(modifier = Modifier.width(8.dp))
+    Text(
+        text = timeToString(task.time),
+        style = MaterialTheme.typography.bodyMedium
+    )
 
-            Text(
-                text = task.name,
-                modifier = Modifier
-                    .weight(1f),
-                style = MaterialTheme.typography.bodyMedium
-            )
 //    if (task.time != null) {
 //        Icon(
 //            imageVector = Icons.Default.Notifications,
@@ -91,49 +88,23 @@ fun TaskItemTest(
 //            modifier = Modifier.weight(0.5f)
 //        )
 //    } OUTDATED (May use later)
-        }
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-        ) {
-            Text(
-                text = task.category.emoji,
-                modifier = Modifier.width(24.dp),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Icon(
-                imageVector = ImageVector.vectorResource(R.drawable.baseline_access_time_16),
-                contentDescription = null
-            )
-            Spacer(modifier = Modifier.width(4.dp))
-            Text(
-                text = timeToString(task.time),
-                style = MaterialTheme.typography.bodyMedium,
-                fontWeight = FontWeight.SemiBold
-            )
-            Row(
-                modifier = Modifier.weight(1f),
-                horizontalArrangement = Arrangement.End,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                when (task.difficulty) {
-                    Difficulty.HARD -> BatteryIcon(color = colorResource(R.color.red))
-                    Difficulty.MEDIUM -> BatteryIcon(
-                        bodyCoefficient = 0.5f,
-                        color = colorResource(R.color.orange)
-                    )
-                    Difficulty.EASY -> BatteryIcon(
-                        bodyCoefficient = 0.25f,
-                        color = colorResource(R.color.green)
-                    )
-                }
-            }
-            //Spacer(modifier = Modifier.width(8.dp))
 
+    Spacer(modifier = Modifier.width(8.dp))
+    Row {
+        when (task.difficulty) {
+            Difficulty.HARD -> BatteryIcon(color = colorResource(R.color.red))
+            Difficulty.MEDIUM -> BatteryIcon(
+                bodyCoefficient = 0.5f,
+                color = colorResource(R.color.orange)
+            )
+
+            Difficulty.EASY -> BatteryIcon(
+                bodyCoefficient = 0.25f,
+                color = colorResource(R.color.green)
+            )
         }
     }
+
 }
 
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
@@ -141,7 +112,7 @@ fun TaskItemTest(
 @Composable
 private fun TaskCardWithIconPreview1() {
     CourseWorkAndroidWeeklyPlannerTheme {
-        TaskItemTest(
+        TaskItemTest1(
             onClick = { },
             task = Task(
                 id = UUID.randomUUID(),
@@ -164,7 +135,7 @@ private fun TaskCardWithIconPreview1() {
 @Composable
 private fun TaskCardWithIconPreview2() {
     CourseWorkAndroidWeeklyPlannerTheme {
-        TaskItemTest(
+        TaskItemTest1(
             onClick = { },
             task = Task(
                 id = UUID.randomUUID(),
@@ -181,3 +152,4 @@ private fun TaskCardWithIconPreview2() {
         )
     }
 }
+
