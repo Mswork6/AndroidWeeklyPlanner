ba944670facb24835b3fc02b02411770ed538974
Mswork6 <denis.ch74@mail.ru>
2025-05-25 01:23:58 +0500

    feat: created confetti animation when completing all tasks at a day

--- app/src/main/java/com/example/courseworkandroidweeklyplanner/presentation/screens/main/component/DayItem.kt
+++ app/src/main/java/com/example/courseworkandroidweeklyplanner/presentation/screens/main/component/DayItem.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -23,13 +24,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.courseworkandroidweeklyplanner.R
 import com.example.courseworkandroidweeklyplanner.domain.model.Day
 import com.example.courseworkandroidweeklyplanner.domain.model.DayType.MONDAY
@@ -52,52 +60,78 @@ fun DayItem(
     )
     val allDone = enabled && day.tasks.all { it.isDone }
 
+    val composition by rememberLottieComposition(LottieCompositionSpec.Asset("confetti.json"))
 
-    ItemCard(
-        shape = MaterialTheme.shapes.large,
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.primary
-        ),
-        onClick = {},
-        modifier = modifier,
-        contentModifier = Modifier
-            .clickable(
-                enabled = enabled,
-                onClick = onClick
-            )
-            .padding(
-                top = 16.dp,
-                bottom = 16.dp,
-                start = 8.dp,
-                end = 8.dp
-            ),
+    val progress by animateLottieCompositionAsState(
+        composition,
+        isPlaying = allDone,
+        iterations = 1,          // проиграть один раз
+        speed = 1.0f
+    )
+
+
+    Box(
+        modifier = modifier
+            .clip(MaterialTheme.shapes.large)
+            .clipToBounds()
     ) {
-        Spacer(modifier = Modifier.width(16.dp))
-        Text(
-            modifier = Modifier
-                .weight(2f),
-            text = stringResource(id = day.type.description),
-            fontWeight = FontWeight.SemiBold
-        )
-        Text(
-            modifier = Modifier.weight(2f),
-            text = dateToString(day.date),
-        )
-        if (enabled) {
-            Icon(
+        ItemCard(
+            shape = MaterialTheme.shapes.large,
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.primary
+            ),
+            onClick = {},
+            contentModifier = Modifier
+                .clickable(
+                    enabled = enabled,
+                    onClick = onClick
+                )
+                .padding(
+                    top = 16.dp,
+                    bottom = 16.dp,
+                    start = 8.dp,
+                    end = 8.dp
+                ),
+        ) {
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
                 modifier = Modifier
-                    .weight(1f)
-                    .rotate(rotationState),
-                imageVector = Icons.Default.KeyboardArrowDown,
-                contentDescription = stringResource(R.string.description_show_hide_tasks)
+                    .weight(2f),
+                text = stringResource(id = day.type.description),
+                fontWeight = FontWeight.SemiBold
             )
-        } else {
-            Row(
-                modifier = Modifier
-                    .weight(1f)
-            ) { }
+            Text(
+                modifier = Modifier.weight(2f),
+                text = dateToString(day.date),
+            )
+            if (enabled) {
+                Icon(
+                    modifier = Modifier
+                        .weight(1f)
+                        .rotate(rotationState),
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = stringResource(R.string.description_show_hide_tasks)
+                )
+            } else {
+                Row(
+                    modifier = Modifier
+                        .weight(1f)
+                ) { }
+            }
+
         }
 
+        if (allDone && composition != null) {
+            LottieAnimation(
+                composition,
+                progress,
+                modifier = Modifier
+                    .matchParentSize()
+                    .clipToBounds(),    // на всю карточку
+                contentScale = ContentScale.FillWidth,
+                clipToCompositionBounds = false
+            )
+        }
     }
 }
 
